9fd09547d0eee8dbe0547189f64f50c9f400d9b2
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-16 00:53:44 -0400

    Npm run start working again

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ function App() {
               </p>
 
               <div class="mt-4 md:mt-8">
-                <a
+                <p
                   href="#"
                   class="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
                 >
                   Get Started Today
-                </a>
+                </p>
               </div>
             </div>
           </div>
