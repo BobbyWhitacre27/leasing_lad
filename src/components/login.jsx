ceefd81c87d5cea48ef906d5bbbf7ea5eefad889
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-18 13:55:45 -0400

    navbar and login updates

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -10,83 +10,71 @@ const Login = () => {
         <section>
 
 
-            <div>
-
-                <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-                    <div class="mx-auto max-w-lg text-center">
-                        <h1 class="text-2xl font-bold sm:text-3xl">Leasing Lad.</h1>
-
-                        <p class="mt-4 text-gray-500">
-                            Keep your sales simple, organized and efficient.
-                        </p>
-                    </div>
+          
 
 
-                    <div class="relative">
-                   
-                        <div class="relative bottom-0 left-0 z-1">
-                            Absolute child
-                        </div>
-                        <video src={leasingLad_video} autoPlay loop muted class="absolute h-30"/>
-                    </div>
+                    {/* main */}
+                    <div>
 
 
+                      
+                        <video src={leasingLad_video} autoPlay loop muted class="absolute content-center brightness-50" />
+                    
 
-
-
-
-
-
-                    <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4 abolute">
-                        <div>
-                            <label for="email" class="sr-only">Email</label>
-
-                            <div class="relative">
-                                <input
-                                    type="email"
-                                    class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                    placeholder="Enter username"
-                                />
-
-
-                            </div>
-                        </div>
-
-                        <div>
-                            <label for="password" class="sr-only">Password</label>
-
-                            <div class="relative">
-                                <input
-                                    type="password"
-                                    class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                    placeholder="Enter password"
-                                />
-
-                            </div>
-                        </div>
-
-                        <div class="flex items-center justify-between">
-                            <p class="text-sm text-gray-500">
-                                <>No account? </>
-                                <Link class="underline" href="/Register">Sign up</Link>
+                    {/* content */}
+                        <div class="relative">
+                            <div>
+                            <h1 class="text-2xl font-bold sm:text-3xl text-white">Leasing Lad.</h1>
+                            <p class="mt-4 text-gray-500 text-white">
+                                Keep your sales simple, organized and efficient.
                             </p>
 
-                            <button
-                                type="submit"
-                                class="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-                            >
-                                Sign in
-                            </button>
+                            <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
+                                <div>
+                                    <div class="relative">
+                                        <input
+                                            type="email"
+                                            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                            placeholder="Enter username"
+                                        />
+
+
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="password" class="sr-only">Password</label>
+
+                                    <div class="relative">
+                                        <input
+                                            type="password"
+                                            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                            placeholder="Enter password"
+                                        />
+
+                                    </div>
+                                </div>
+
+                                <div class="flex items-center justify-between">
+                                    <p class="text-sm text-white">
+                                        <>No account? </>
+                                        <Link class="underline" href="/Register">Sign up</Link>
+                                    </p>
+
+                                    <button
+                                        type="submit"
+                                        class="inline-block rounded-lg bg-black px-5 py-3 text-sm font-medium text-white"
+                                    >
+                                        Sign in
+                                    </button>
+                                </div>
+                            </form>
+                            </div>
                         </div>
-                    </form>
+                    </div>
 
 
 
-
-
-
-                </div>
-            </div>
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", './src/App.jsx'],
+  content: ["./public/index.html", './src/App.jsx', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
