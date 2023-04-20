cc22513cd910eeb93114c939a77275bc636cc3eb
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-19 23:55:56 -0400

    Future move in page updates

--- src/components/upcoming_moves.jsx
+++ src/components/upcoming_moves.jsx
@@ -1,12 +1,96 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Link, Route, Routes, Switch, useNavigate  } from "react-router-dom";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 import { handle } from "../..";
 import { getAllResident_Cards } from '../api';
 
 const Upcoming_moves = () => {
-    const navigate = useNavigate();
-
     const [resident_cards, setResident_Cards] = useState([]);
+    const [select, setSelect] = useState(false);
+    const [selectedId, setSelectedId] = useState('')
+
+
+
+    const [name, setName] = useState('');
+    const [apartment, setApartment] = useState('');
+    const [rent, setRent] = useState('');
+    const [leaseTerm, setLeaseTerm] = useState('');
+    const [moveDate, setMoveDate] = useState('');
+    const [approvalDocsSent, setApprovalDocsSent] = useState(false);
+    const [leaseSent, setLeaseSent] = useState(false);
+    const [electricSetUp, setElectricSetUp] = useState(false);
+    const [insuranceSetUp, setInsuranceSetUp] = useState(false);
+    const [leaseSigned, setLeaseSigned] = useState(false);
+    const [paymentMade, setPaymentMade] = useState(false);
+    const [notes, setNotes] = useState('');
+    const [movedIn, setMovedIn] = useState(false);
+
+
+    const handleName = event => {
+        setName(event.target.value)
+    }
+
+    const handleApartment = event => {
+        setApartment(event.target.value)
+    }
+
+    const handleRent = event => {
+        setRent(event.target.value)
+    }
+
+    const handleLeaseTerm = event => {
+        setLeaseTerm(event.target.value)
+    }
+
+    const handleApprovalDocsSent = event => {
+        if (approvalDocsSent === false) {
+            return setApprovalDocsSent(true)
+        }
+        setApprovalDocsSent(false)
+    }
+
+    const handleLeaseSent = event => {
+        if (leaseSent === false) {
+            return setLeaseSent(true)
+        }
+        setLeaseSent(false)
+    }
+
+    const handleElectricSetUp = event => {
+        if (electricSetUp === false) {
+            return setElectricSetUp(true)
+        }
+        setElectricSetUp(false)
+    }
+
+    const handleInsuranceSetUp = event => {
+        if (insuranceSetUp === false) {
+            return setInsuranceSetUp(true)
+        }
+        setInsuranceSetUp(false)
+    }
+
+    const handleLeaseSigned = event => {
+        if (leaseSigned === false) {
+            return setLeaseSigned(true)
+        }
+        leaseSigned(false)
+    }
+
+    const handlePaymentMade = event => {
+        if (paymentMade === false) {
+            return setPaymentMade(true)
+        }
+        setPaymentMade(false)
+    }
+
+    const handleNotes = event => {
+        setNotes(event.target.value)
+    }
+ 
+
+
 
     const cards = async () => {
         const cards = await getAllResident_Cards();
@@ -15,53 +99,111 @@ const Upcoming_moves = () => {
     console.log({resident_cards})
 
 
-    const handleSelect = (residentCardId) => {
-        navigate("/Resident_card", {residentCardId: residentCardId})
+    const handleSelect = (id) => {
+        if(select === false) {
+            setSelect(true)
+            setSelectedId(id)
+            return
+        }
+        setSelect(false)
+        setSelectedId("")
     }
 
-console.log({navigate})
 
     const residentCard = resident_cards.map((c) => {
 
         const date = new Date(c.move_in_date)
         const newDate = date.toLocaleDateString('en-US');
-        const residentCardId = c.id
-        console.log({residentCardId})
         
-        return <div onClick={() => handleSelect(residentCardId)}>
-            {/* <Link navigate() residentCardId={residentCardId} to="/Resident_card"> */}
-            <a class="group relative block h-64 sm:h-64 lg:h-64 w-80 mt-8 mx-4">
-                <span class="absolute inset-0 border-2 border-dashed border-black"></span>
+        return <div class="mb-4 mt-4">
+            <button onClick={(event) => handleSelect(c.id)} class="flex justify-between w-full font-bold bg-black text-white border-black border-2 p-4 rounded-3xl">
+                
+                <h1>APT #{c.apartment}</h1>
+                <h1>{c.name}</h1>
+                <h1>{newDate}</h1>
+                <h1>80%</h1>
+                {select === true & selectedId === c.id ? <h1>Close</h1> : <h1>Details</h1>}
 
-                <div
-                    class="relative h-full border-2 border-black bg-white group-hover:-translate-x-2 group-hover:-translate-y-2"
-                >
-                    <div
-                        class=" !pt-0 group-hover:absolute group-hover:opacity-0"
-                    >
+            </button>
+
+            {select === true & selectedId === c.id ? <div>
+
+                <section class="mb-8 mt-8">
+            <div class="w-3/4 m-auto">
+            
 
 
-                        <div class="text-left h-full align-center text-black">
-                            <div class="flex justify-between bg-black text-white p-2 pb-4"><h2 class="mt-4 font-bold text-3xl">#{c.apartment}</h2><h2 class="mt-4 font-bold text-3xl">{newDate}</h2></div>
-                            
-                            
-                            <div class="flex justify-between p-2"><h2 class="mt-4 font-bold text-3xl">{c.name}</h2><h2 class="mt-4 font-bold text-3xl">80%</h2></div>
+            <div class="rounded-lg mt-8 bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                <div class="flex justify-between">
+                
+                
+                
+                <h1 class="text-black text-3xl font-bold">{c.name}</h1>
+                    <button class="text-white bg-black p-2 rounded-xl mb-2" onClick={handleSelect}>Close</button>
+                    </div>
+                            <h1 class="font-bold text-red-600 mb-2"></h1>
+                            <form action="" class="space-y-4">
+
+                            <div class="grid justify-start text-left mt-4">
+                                    <h1 class="font-bold text-xl">Leasing Agent Tasks:</h1>
+                                    <div class="flex mt-2"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handleApprovalDocsSent} /><h1>Reservation Agreement & Approval Letter Sent?</h1></div>
+                                    <div class="flex mt-2"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handleLeaseSent} /><h1>Lease Sent?</h1></div>
+                                </div>
+
+                                <hr></hr>
+
+                                <div class="grid justify-start text-left">
+                                    <h1 class="font-bold text-xl">Future Resident Tasks:</h1>
+                                    <div class="flex mt-2"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handleElectricSetUp} /><h1>Electric set-up?</h1></div>
+                                    <div class="flex mt-2"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handleInsuranceSetUp} /><h1>Renter's Insurance set-up?</h1></div>
+                                    <div class="flex mt-2"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handleLeaseSigned} /><h1>Lease signed?</h1></div>
+                                    <div class="flex mt-2"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handlePaymentMade} /><h1>Paid move-in funds?</h1></div>
+                                </div>
+
+                                <div>
+                                <h1 class="text-left font-bold text-xl">Notes:</h1>
+                                    <div class="text-left mb-2">{c.notes}</div>
+                                    <label class="sr-only" for="message" >Account Notes</label>
+
+                                    <textarea
+                                        class="w-full rounded-lg border-black border-1 p-3 text-sm"
+                                        placeholder="Account Notes"
+                                        rows="8"
+                                        id="message"
+                                        onChange={handleNotes}
+                                    ></textarea>
+                                </div>
+
+
+                                <div class="flex justify-between">
+                                <button
+                                        type="submit"
+                                        class="inline-block w-full rounded-lg border-black border-2 bg-white px-5 py-3 font-medium text-black sm:w-auto"
+                                        
+                                    >
+                                        Delete Resident
+                                    </button>
+                                <button
+                                        type="submit"
+                                        class="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+                                        
+                                    >
+                                        Update notes
+                                    </button>
+                                </div>
+                                <div class="flex mt-6 justify-center"><input type="checkbox" class="mr-2 w-5 h-5" onClick={handleApprovalDocsSent} /><h1>Moved-in?</h1></div>
+
+                                <div class="mt-2">
+        
+                                </div>
+                            </form>
                         </div>
 
-                    </div>
 
-                    <div
-                        class="absolute p-2 opacity-0 group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8"
-                    >
-                        
-            
-                        <h1 class="mt-14 text-xl text-black font-bold sm:text-xl ">Lease Term: {c.lease_term} Months</h1>
-                        <h1 class="text-xl text-black font-bold sm:text-xl ">Rent: ${c.rent}</h1>
-                        <h1 class="mt-6 text-xl text-black font-bold sm:text-xl ">See Details</h1>
-                    </div>
-                </div>
-            </a>
-        
+            </div>
+        </section>
+                
+            </div> : ""}
         </div>
 
      
@@ -78,7 +220,7 @@ console.log({navigate})
 
 
     return (
-        <section class="mb-8 mt-8">
+        <section class="mb-12 mt-8">
             <div class="w-3/4 m-auto">
                 <h1 class="text-black text-5xl font-bold">Future Move-in's</h1>
                 
@@ -91,9 +233,10 @@ console.log({navigate})
                     </button>
                 </Link>
 
-        <div className="cardsDisplay">
+        <div class="mt-8">
                 {residentCard}
                </div>
+               
             </div>
         </section>
     )
