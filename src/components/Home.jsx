58cf76e28e1740e6e428f69f0eae9c8ccbbdb540
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-24 01:11:25 -0400

    video demo added to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import leasingLad_video from "../video/leasingLad_video.mov"
+import leasingLadDemo from "../video/leasingLadDemo2.mov"
 
 const Home = () => {
 
@@ -10,7 +11,7 @@ const Home = () => {
                 <div>
 
                     <div class="">
-                    <video src={leasingLad_video} autoPlay loop muted plays-inline class="brightness-50 object-cover h-72 w-full" />
+                        <video src={leasingLad_video} autoPlay loop muted plays-inline class="brightness-50 object-cover h-72 w-full" />
                     </div>
 
                     <div>
@@ -45,7 +46,7 @@ const Home = () => {
                                     </dt>
 
                                     <dd class="flex justify-center text-4xl font-extrabold text-black md:text-5xl">
-                                    <img class="h-16" src="https://cdn-icons-png.flaticon.com/512/1167/1167092.png"></img>
+                                        <img class="h-16" src="https://cdn-icons-png.flaticon.com/512/1167/1167092.png"></img>
                                     </dd>
                                 </div>
 
@@ -53,11 +54,11 @@ const Home = () => {
                                     class="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center"
                                 >
                                     <dt class="order-last text-lg mt-4 font-medium text-gray-500">
-                                        Easier way to organize. 
+                                        Easier way to organize.
                                     </dt>
 
                                     <dd class="flex justify-center text-4xl font-extrabold text-black md:text-5xl">
-                                    <img class="h-16" src="https://media.istockphoto.com/id/1186273390/vector/cartoon-flat-style-vintage-analog-clock-icon-time-symbol-logo-vector-illustration-image.jpg?s=170667a&w=0&k=20&c=7imSeeUMkHfKg3h78n2l7cxs472v1cavR2ShMhPA1qo="></img>
+                                        <img class="h-16" src="https://media.istockphoto.com/id/1186273390/vector/cartoon-flat-style-vintage-analog-clock-icon-time-symbol-logo-vector-illustration-image.jpg?s=170667a&w=0&k=20&c=7imSeeUMkHfKg3h78n2l7cxs472v1cavR2ShMhPA1qo="></img>
                                     </dd>
                                 </div>
 
@@ -69,7 +70,7 @@ const Home = () => {
                                     </dt>
 
                                     <dd class="flex justify-center text-4xl font-extrabold text-black md:text-5xl">
-                                    <img class="h-16" src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-3-1/512/dollar-512.png"></img>
+                                        <img class="h-16" src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-3-1/512/dollar-512.png"></img>
                                     </dd>
                                 </div>
                             </dl>
@@ -77,7 +78,25 @@ const Home = () => {
                     </div>
                 </section>
 
-
+                <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">
+                    How Leasing Lad Works?
+                </h2>
+                <div class="flex justify-center m-8">
+                    <video src={leasingLadDemo} autoPlay loop muted plays-inline class="object-cover  w-1/2" />
+                </div>
+                <h2 class="text-xl text-gray-900  mb-8">
+                    <ul>
+                        <li>1. Create an account</li>
+                        <li>2. Add new residents to your folders</li>
+                        <li>3. Update your paper work status</li>
+                        <li>4. Keep status on file</li>
+                        
+                    </ul>
+                    <h2 class="text-xl font-bold text-gray-900 mt-4">
+                       ** Bonus: Rent Prorator and Rent Specials Calculator incluced **
+                </h2>
+                
+                </h2>
             </section>
         </section>
     )
